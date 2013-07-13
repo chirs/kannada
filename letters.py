680b909a2db58cabc2b93666a648d4c73b7214f3
Chris Edgemon <chrisedgemon@gmail.com>
2013-07-13 06:20:59 -0400

    Add Kannada-English letter mapping.

---
+++ letters.py
@@ -0,0 +1,71 @@
+
+
+vowels = {
+    'ಅ': 'a', #KANNADA LETTER A
+    'ಆ': 'ā', #KANNADA LETTER AA
+    'ಇ': 'i', #KANNADA LETTER E
+    'ಈ': 'ī', #KANNADA LETTER EE
+    'ಉ': 'u', #KANNADA LETTER U
+    'ಊ': 'ū', #KANNADA LETTER UU
+    'ಋ': 'r̥', #KANNADA LETTER VOCALIC R
+    'ೠ': 'r̥̄', #KANNADA LETTER VOCALIC RR[1]
+    'ಎ': 'e', #KANNADA LETTER E
+    'ಏ': 'ē', #KANNADA LETTER EE
+    'ಐ': 'a', #iKANNADA LETTER AI
+    'ಒ': 'o', #KANNADA LETTER O
+    'ಓ': 'ō', #KANNADA LETTER OO
+    'ಔ': 'au', #KANNADA LETTER AU
+}
+
+numerals = {
+    '೦': '0', #sonne (ಸೊನ್ನೆ)0Zero
+    '೧': '1', #ondu (ಒಂದು)1One
+    '೨': '2', #eraḍu (ಎರಡು)2Two
+    '೩': '3', #mūru (ಮೂರು)3Three
+    '೪': '4', #nālku (ನಾಲ್ಕು)4Four
+    '೫': '5', #aidu (ಐದು)5Five
+    '೬': '6', #āru (ಆರು)6Six
+    '೭': '7', #ēḷu (ಏಳು)7Seven
+    '೮': '8', #enṭu (ಎಂಟು)8Eight
+    '೯': '9', #oṃbattu (ಒಂಬತ್ತು)9Nine
+}
+
+consonants = {
+
+    # voiceless, voiceless aspirate, voiced, voiced aspirate, nasal
+
+    #Velars
+    'ಕ': 'ka',
+    'ಖ': 'kha',
+    'ಗ': 'ga',
+    'ಘ': 'gha',
+    'ಙ': 'nga',
+
+    #Palatals
+    'ಚ': 'cha',
+    'ಛ': 'chha',
+    'ಜ': 'ja',
+    'ಝ': 'jha',
+    'ಞ': 'nya', #'IAST ña, Unicode nya',
+
+    #Retroflex
+    'ಟ': 'tta', #'IAST ṭa, Unicode tta',
+    'ಠ': 'ttha', #'IAST ṭha, Unicode ttha',
+    'ಡ': 'dda', #'IAST ḍa, Unicode dda',
+    'ಢ': 'ddha', #'IAST ḍha, Unicode ddha',
+    'ಣ': 'nna', #'IAST ṇa, Unicode nna',
+
+    #Dentals
+    'ತ': 'ta',
+    'ಥ': 'tha',
+    'ದ': 'da',
+    'ಧ': 'dha',
+    'ನ': 'na',
+
+    #Labials
+    'ಪ': 'pa',
+    'ಫ': 'pha',
+    'ಬ': 'ba',
+    'ಭ': 'bha',
+    'ಮ': 'ma',
+}
